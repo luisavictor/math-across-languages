ce95c14ae0a82335b84c15c03c79ae76768ad6eb
victorl0 <s6lavict@uni-bonn.de>
2025-11-03 14:45:38 +0100

    translated math datasets and attempt to translate race

--- MathNeuro/run_mathneuro.py
+++ MathNeuro/run_mathneuro.py
@@ -14,18 +14,21 @@ venv_python = r"C:\Users\thmsr\Desktop\LabTest\.venv\Scripts\python.exe"
 # --calibration_datasets --> datasets used to compute parameter importance for non-math tasks
 # --train_lm_eval_task --> only relevant if the train dataset is an LM evaluation harness task (like GSM8K). It tells MathNeuro which built-in task evaluation format to use (CoT prompting, etc.).
 
+
+
+
 cmd = [
     venv_python,
     r"C:\Users\thmsr\Desktop\LabTest\MathNeuro\MathNeuro.py",  # full path to script
-    "--model", "meta-llama/Llama-3.2-1B",  # which model to use?
+    "--model", "meta-llama/Llama-3.2-1B-Instruct",  # which model to use?
     "--save_path", "results_path",  # where to store
     "--train_dataset", "data/gsm8k.csv",  #data to find task-important params
     "--eval_datasets", "race",   # data to evaluate catastrophic forgetting after pruning/scaling
     "--calibration_datasets", "data/race.csv",  #data to find important non-task params
     "--calibration_dataset_names", "Race",
-    "--eval_dataset_subset", "1",
+    "--eval_dataset_subset", "2",
     "--train_lm_eval_task", "gsm8k_cot", # for LM evaluation harness tasks
-    #"--pre_train_eval",
+    "--pre_train_eval",
     #"--scalar", "0.01",
     #"--eval_dataset_size", "1",
     #"--num_samples", "1",
